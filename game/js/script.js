0e51a2b8e112de9243e7e82bf3c90acb8f81ab14
elouanlerouxel <72791170+elouanlerouxel@users.noreply.github.com>
2021-02-02 14:37:21 +0000

    HighScoreTable

--- game/js/script.js
+++ game/js/script.js
@@ -168,6 +168,10 @@ function gameloop() {
             localStorage.setItem(usernameClient, score);
             displayScore.innerHTML = "New High Score!";
             username.innerHTML = "Hi " + usernameClient + "!<br>Your high score is " + localStorage.getItem(usernameClient);
+            for ( var i = 0; i < localStorage.length; i++) {
+                rank += localStorage.key(i) + " : " + localStorage.getItem(localStorage.key(i)) + "<br>";
+            }
+document.getElementById("rank").innerHTML = rank;
         } else {
             displayScore.innerHTML = score;
         }
@@ -240,10 +244,11 @@ if (urlParam.has("username")) {
 if (localStorage.getItem(usernameClient) === null) {
     localStorage.setItem(usernameClient,score);
 }
-
-for ( var i = 0, len = localStorage.length; i < len; i++) {
-  console.log(localStorage.getItem(localStorage.key(i)));
+var rank;
+for ( var i = 0; i < localStorage.length; i++) {
+    rank += localStorage.key(i) + " : " + localStorage.getItem(localStorage.key(i)) + "<br>";
 }
+document.getElementById("rank").innerHTML = rank;
 
 username.innerHTML = "Hi " + usernameClient + "!<br>Your high score is " + localStorage.getItem(usernameClient) + " seconds";
 
