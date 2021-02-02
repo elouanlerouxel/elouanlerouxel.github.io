4f6a47b137a9abf89a66eb0e8307ef703598ef53
elouanlerouxel <72791170+elouanlerouxel@users.noreply.github.com>
2021-02-02 14:51:32 +0000

    Update script.js

--- game/js/script.js
+++ game/js/script.js
@@ -246,12 +246,10 @@ if (localStorage.getItem(usernameClient) === null) {
     localStorage.setItem(usernameClient,score);
 }
 
-var rank;
+var rank = "";
 for ( var i = 0; i < localStorage.length; i++) {
-    console.log(localStorage.key(i));
     rank += localStorage.key(i) + " : " + localStorage.getItem(localStorage.key(i)) + "<br>";
 }
-console.log(rank);
 document.getElementById("rank").innerHTML = rank;
 
 username.innerHTML = "Hi " + usernameClient + "!<br>Your high score is " + localStorage.getItem(usernameClient) + " seconds";
